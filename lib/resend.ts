44651228e9ad784301be61293c42ac89e919622f
Favour Enwonwu <favourenwonwukc@gmail.com>
2026-09-11 10:13:38 +0100

    form submission

--- lib/resend.ts
+++
@@ -1,6 +0,0 @@
-import { Resend } from "resend";
-
-export const resend = new Resend(process.env.RESEND_API_KEY);
-
-export const DOCTOR_EMAIL = process.env.DOCTOR_EMAIL || "sundayokafor60.com";
-export const FROM_EMAIL = process.env.FROM_EMAIL || "onboarding@resend.dev";
